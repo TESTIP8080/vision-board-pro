a7491f93aa9002ad590f317dd558533320fe5be9
TESTIP8080 <lmo.markets@example.com>
2025-06-14 19:45:13 +0600

    fix: use supported image size 1024x1024 for DALL-E 3

--- api/generate-image.js
+++ api/generate-image.js
@@ -35,7 +35,7 @@ export default async function handler(req, res) {
         model: 'dall-e-3',
         prompt: dallEPrompt,
         n: 1,
-        size: '512x512',
+        size: '1024x1024',
       }),
     });
 
